c4a11ba8e94141def7440df0d90e0a58ada58f2a
Mile Prastalo <mile.prastalo97@gmail.com>
2024-06-15 13:48:21 +0200

    trying again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatTabsModule} from '@angular/material/tabs';
-import { AboutmeComponent } from './aboutme/aboutme.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatTabsModule ,MatToolbarModule,
-     AboutmeComponent, HomeComponent, ContactComponent, MatCardModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'mileprastalowebsite';
+  title = 'mileprastalo-website';
 }
