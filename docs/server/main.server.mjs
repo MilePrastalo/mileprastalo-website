51b4ab144d66391def8a07bc97be55c5686a6c25
Mile Prastalo <mile.prastalo97@gmail.com>
2024-06-15 15:16:48 +0200

    new build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2LHBZAAQ.mjs";import"./chunk-4GL6AEI7.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-NUTW6RCA.mjs";import"./chunk-4GL6AEI7.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
